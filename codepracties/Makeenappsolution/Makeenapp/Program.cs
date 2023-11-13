9381cc19333678e38927dbc4a25be20c595377e0
salim <salimyaaqoubii@gmail.com>
2023-11-13 16:52:02 +0400

    new project

---
+++ codepracties/Makeenappsolution/Makeenapp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Makeenapp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string message = "Hello i am salim al yaaqoubi!!";
+
+            Console.WriteLine(message);
+        }
+    }
+}
